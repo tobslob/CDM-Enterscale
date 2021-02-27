3e45e9141a9237081d0c0d842b758a1caa23f368
tobslob <kazmobileaap@gmail.com>
2021-02-27 10:18:16 +0100

    fix: manual fix to github folder structure

--- src/data/User/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./user.model";
-export * from "./user.repo";
-export * from "./user.schema";
--- src/server/controllers/Session/session.validator.ts
+++
@@ -1,7 +0,0 @@
-import { JoiValidator } from "@app/data/util/validate";
-import joi from "@hapi/joi";
-
-export const isLoginDTO = joi.object({
-  email_address: JoiValidator.validateEmail(),
-  password: JoiValidator.validatePassword()
-});
