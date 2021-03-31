896da59d6f820a9f346f57be6b15220397ad5872
tobslob <kazmobileaap@gmail.com>
2021-03-31 13:32:03 +0100

    feat: add edit campaign endpoint

--- src/server/controllers/campaign/campaign.controller.ts
+++ src/server/controllers/campaign/campaign.controller.ts
@@ -6,7 +6,8 @@ import {
   httpGet,
   requestBody,
   requestParam,
-  httpDelete
+  httpDelete,
+  httpPatch
 } from "inversify-express-utils";
 import { BaseController, validate, ConstraintError } from "@app/data/util";
 import { Request, Response } from "express";
@@ -14,7 +15,7 @@ import { Campaign, CampaignDTO, CampaignRepo } from "@app/data/campaign";
 import { canCreateCampaign } from "./campaign.middleware";
 import { SendMessageResponse } from "africastalking-ts";
 import { isCampaignDTO } from "./campaign.validator";
-import { differenceInDays } from "date-fns";
+import { differenceInCalendarDays } from "date-fns";
 
 type ControllerResponse = Campaign[] | Campaign | SendMessageResponse | any;
 
@@ -27,7 +28,7 @@ export class CampaignController extends BaseController<ControllerResponse> {
       const user = req.session.user;
 
       if (body.end_date) {
-        const diff = differenceInDays(body.end_date, body.start_date);
+        const diff = differenceInCalendarDays(body.end_date, body.start_date);
         if (diff < 1) {
           throw new ConstraintError("The end date must at least be a day after start date");
         }
@@ -71,6 +72,30 @@ export class CampaignController extends BaseController<ControllerResponse> {
       const campaigns = await CampaignRepo.destroy({ workspace, _id });
 
       this.handleSuccess(req, res, campaigns);
+    } catch (error) {
+      this.handleError(req, res, error);
+    }
+  }
+
+  @httpPatch("/:id", canCreateCampaign, validate(isCampaignDTO))
+  async editCampaign(
+    @request() req: Request,
+    @response() res: Response,
+    @requestParam("id") id: string,
+    @requestBody() body: CampaignDTO
+  ) {
+    try {
+      const workspace = req.session.workspace;
+
+      if (body.end_date) {
+        const diff = differenceInCalendarDays(body.end_date, body.start_date);
+        if (diff < 1) {
+          throw new ConstraintError("The end date must at least be a day after start date");
+        }
+      }
+
+      const campaign = await CampaignRepo.editCampaign(workspace, id, body);
+      this.handleSuccess(req, res, campaign);
     } catch (error) {
       this.handleError(req, res, error);
     }
