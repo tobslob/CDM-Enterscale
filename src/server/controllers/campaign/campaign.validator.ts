6a81923e1c5eecdc8cd26d218e4a43c768b89d88
tobslob <kazmobileapp@gmail.com>
2021-04-22 12:50:16 +0100

    feat: support bulk campaign selection

--- src/server/controllers/campaign/campaign.validator.ts
+++ src/server/controllers/campaign/campaign.validator.ts
@@ -4,12 +4,25 @@ import joi from "@hapi/joi";
 export const isCampaignDTO = joi.object({
   name: JoiValidator.validateString().required(),
   description: JoiValidator.validateString().required(),
-  channel: JoiValidator.validateString().valid("FACEBOOK", "TWITTER", "EMAIL", "SMS", "INSTAGRAM").required(),
+  channel: JoiValidator.validateString()
+    .valid("FACEBOOK", "TWITTER", "EMAIL", "SMS", "INSTAGRAM", "VOICE", "LINKEDIN")
+    .required(),
   amount: JoiValidator.validateNumber().required(),
   frequency: JoiValidator.validateString().valid("DAILY", "WEEKLY", "MONTHLY").required(),
-  start_date: JoiValidator.validDate(),
+  start_date: JoiValidator.validDate().required(),
   end_date: JoiValidator.validDate(),
   target_audience: JoiValidator.validateString().required(),
   message: JoiValidator.validateString().required(),
   organisation: JoiValidator.validateString()
+});
+
+export const isCampaignQuery = joi.object({
+  name: JoiValidator.validateString(),
+  subject: JoiValidator.validateString(),
+  frequency: JoiValidator.validateString().valid("DAILY", "WEEKLY", "MONTHLY"),
+  from: JoiValidator.validDate(),
+  to: JoiValidator.validDate(),
+  organisation: JoiValidator.validateString(),
+  channel: JoiValidator.validateString().valid("FACEBOOK", "TWITTER", "EMAIL", "SMS", "INSTAGRAM", "VOICE", "LINKEDIN"),
+  description: JoiValidator.validateString()
 });
