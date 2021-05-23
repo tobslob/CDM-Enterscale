eb89416ca054c07891e6624b9068f640dcb0fa18
tobslob <kazmobileapp@gmail.com>
2021-05-23 23:29:06 +0100

    fix: sleepy commit

--- src/data/defaulter/defaulter.repo.ts
+++ src/data/defaulter/defaulter.repo.ts
@@ -78,7 +78,7 @@ class DefaulterRepository extends BaseRepository<Defaulters> {
     await DefaulterRepo.destroy(defaulter.id);
   }
 
-  async editDefulter(workspace: string, id: string, defaulter: Defaulters) {
+  async editDefulter(workspace: string, id: string, defaulter: any) {
     return this.atomicUpdate(
       {
         _id: id,
