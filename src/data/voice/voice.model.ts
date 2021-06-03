6cc60b199b7aefc08aa085a6d43c51492a6154ee
tobslob <kazmobileapp@gmail.com>
2021-06-03 19:47:10 +0100

    fix: normalise data

--- src/data/voice/voice.model.ts
+++ src/data/voice/voice.model.ts
@@ -2,28 +2,25 @@ import { Model } from "@random-guys/bucket";
 
 export interface Voice extends Model {
   callback_url?: string;
-  data?: {
-    id?: string;
-    ref_id?: string;
-    recipient?: string;
-    caller_id?: string;
-    status?: string;
-    price?: string;
-    account_balance?: string;
-    error_code?: string;
-    error_reason?: string;
-    call_start_time?: string;
-    call_end_time?: string;
-    call_connect_time?: string;
-    media_duration?: string;
-    key_pressed?: string;
-    media_url?: string;
-    workspace?: string;
-    api_token?: string;
-    event_timestamp?: string;
-    queued?: string;
-    timestamp?: string;
-  };
+  call_id?: string;
+  ref_id?: string;
+  recipient?: string;
+  caller_id?: string;
+  status?: string;
+  price?: string;
+  account_balance?: string;
+  error_code?: string;
+  error_reason?: string;
+  call_start_time?: string;
+  call_end_time?: string;
+  call_connect_time?: string;
+  media_duration?: string;
+  key_pressed?: string;
+  media_url?: string;
+  workspace?: string;
+  event_timestamp?: string;
+  queued?: string;
+  timestamp?: string;
   api_token?: string;
   cmd?: string;
 }
