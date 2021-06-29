01e4f50268fd2de908597582aa7bbdae70a3167e
tobslob <kazmobileapp@gmail.com>
2021-06-29 16:35:26 +0100

    fix: wrong function name and constraint string

--- src/server/controllers/cloudinary/cloudinary.controller.ts
+++ src/server/controllers/cloudinary/cloudinary.controller.ts
@@ -4,14 +4,14 @@ import { controller, httpPost, request, response } from "inversify-express-utils
 import { BaseController } from "@app/data/util";
 import { isUpload } from "./cloudinary.middleware";
 import { canCreateDefaulters } from "../defaulter/defaulter.middleware";
-import uploadImage from "@app/services/upload";
+import uploadMedia from "@app/services/upload";
 
 @controller("/uploads")
 export class UploadController extends BaseController<string[]> {
   @httpPost("/file", canCreateDefaulters, isUpload)
   async UploadToCloudinary(@request() req: Request, @response() res: Response) {
     try {
-      await uploadImage(req, res);
+      await uploadMedia(req, res);
       this.handleSuccess(req, res, req["urls"]);
     } catch (error) {
       this.handleError(req, res, error);
