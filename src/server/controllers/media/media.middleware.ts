a382917dd8a4fb79efc64ff29740230e9d40da80
tobslob <kazmobileapp@gmail.com>
2021-06-30 19:45:47 +0100

    fix: wrong variable name

--- src/server/controllers/media/media.middleware.ts
+++ src/server/controllers/media/media.middleware.ts
@@ -5,12 +5,12 @@ const PNG_MIME = "image/png";
 const JPEG_MIME = "image/jpeg";
 const MP3_MINE = "audio/mpeg";
 const MP4_MINE = "video/mp4";
-const TEN_MEGABYTES = 3 * 1024 * 1024;;
+const THREE_MEGABYTES = 3 * 1024 * 1024;;
 
 const storage = multer.memoryStorage();
 const upload = multer({
   storage: storage,
-  limits: { fileSize: TEN_MEGABYTES, files: 1 },
+  limits: { fileSize: THREE_MEGABYTES, files: 1 },
   fileFilter(_req, file, cb) {
     if (file.mimetype === PNG_MIME || file.mimetype === JPEG_MIME || MP3_MINE || MP4_MINE) {
       return cb(null, true);
