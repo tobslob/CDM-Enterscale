f10e2a246201b3ea3096144ef96877a02ff618c1
tobslob <kazmobileapp@gmail.com>
2021-07-16 11:03:16 +0100

    fix: unable to upload files

--- src/common/services/authorisation/authoriser.ts
+++ src/common/services/authorisation/authoriser.ts
@@ -3,31 +3,6 @@ import { FORBIDDEN } from "http-status-codes";
 
 export type RequestPredicate = (req: Request) => void;
 
-/**
- * Create a middleware that runs all the predicates passed and fails with `FailedPredicateError` when
- * any one of them fails. If scheme is not null, it will not run any predicate for a request whose
- * authorisation uses that scheme
- * @param predicate function to test a request
- * @param scheme authorisation scheme to ignore
- */
-export function checkRequest(scheme: string | null, ...predicates: RequestPredicate[]): RequestHandler {
-  return (req, _res, next) => {
-    // ignore headless requests
-    if (scheme && req.headers.authorization) {
-      const [reqScheme] = req.headers.authorization.split(" ");
-      if (scheme.toLowerCase() === reqScheme.toLowerCase()) {
-        return next();
-      }
-    }
-
-    for (const p of predicates) {
-      p(req);
-    }
-
-    next();
-  };
-}
-
 export const sessionPermissions = <const>["super_admin", "loan_admin", "users"];
 export type Permission = typeof sessionPermissions[number];
 
--- src/server/controllers/media/media.middleware.ts
+++ src/server/controllers/media/media.middleware.ts
@@ -1,29 +1,33 @@
 import multer from "multer";
-// import { ConstraintError } from "@app/data/util";
 import uuid from "uuid/v4";
 import { UnSupportedFileError } from "@app/data/util";
 
 const PNG_MIME = "image/png";
 const JPEG_MIME = "image/jpeg";
+const JPG_MIME = "image/jpg";
 const MP3_MINE = "audio/mpeg";
 const MP4_MINE = "video/mp4";
-const FIVE_MEGABYTES = 5 * 1024 * 1024;;
+const FIVE_MEGABYTES = 5 * 1024 * 1024;
 
 const storage = multer.diskStorage({
   filename: (_req, file, cb) => {
     const fileExt = file.originalname.split(".").pop();
     const filename = `${uuid()}.${fileExt}`;
     cb(null, filename);
-  },
+  }
 });
-
-const fileType = PNG_MIME || JPEG_MIME || MP3_MINE || MP4_MINE
 
 const upload = multer({
   storage: storage,
   limits: { fileSize: FIVE_MEGABYTES, files: 1 },
   fileFilter(_req, file, cb) {
-    if (file.mimetype == fileType) {
+    if (
+      file.mimetype == PNG_MIME ||
+      file.mimetype == JPEG_MIME ||
+      file.mimetype == JPG_MIME ||
+      file.mimetype == MP3_MINE ||
+      file.mimetype == MP4_MINE
+    ) {
       return cb(null, true);
     }
 
